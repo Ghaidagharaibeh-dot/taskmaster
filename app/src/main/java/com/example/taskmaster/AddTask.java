e886038e2e5de04280434d760d4d31747d52dc7d
Ghaidagharaibeh-dot <ghedaghraibeh@yahoo.com>
2021-11-20 18:02:12 +0200

    update

--- app/src/main/java/com/example/taskmaster/AddTask.java
+++ app/src/main/java/com/example/taskmaster/AddTask.java
@@ -1,2 +1,29 @@
-package com.example.taskmaster;public class AddTask {
+package com.example.taskmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class AddTask extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+
+        Button addTasks= (Button) findViewById(R.id.addTasks);
+
+        addTasks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"saved",Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+
+
 }
