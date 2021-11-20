785fca26561711c0b8d904c4238262ff4adec764
Ghaidagharaibeh-dot <ghedaghraibeh@yahoo.com>
2021-11-20 18:19:24 +0200

    finaly lab28

--- app/src/main/java/com/example/taskmaster/Setting.java
+++ app/src/main/java/com/example/taskmaster/Setting.java
@@ -18,7 +18,7 @@ public class Setting extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
-        Button save =(Button) findViewById(R.id.saveButton);
+        Button save = (Button) findViewById(R.id.saveButton);
 
         save.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,16 +27,15 @@ public class Setting extends AppCompatActivity {
 
                 String userName = editText.getText().toString();
                 SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(Setting.this);
-                sharedPreferences.edit().putString("userNme",userName).apply();
+                sharedPreferences.edit().putString("userNme", userName).apply();
 
-                Toast.makeText(getApplicationContext(),"saved",Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "saved", Toast.LENGTH_LONG).show();
             }
         });
 
 
-
-
     }
+}
 
 
 
--- app/src/main/java/com/example/taskmaster/TaskDetails.java
+++ app/src/main/java/com/example/taskmaster/TaskDetails.java
@@ -12,18 +12,18 @@ public class TaskDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_details);
 
-        String taskName=getIntent().getStringExtra("taskname");
-        TextView tasktitle=findViewById(R.id.titleDetails);
+        String taskName = getIntent().getStringExtra("taskname");
+        TextView tasktitle = findViewById(R.id.titleDetails);
         tasktitle.setText(taskName);
 
 
-        String taskBody=getIntent().getStringExtra("taskBody");
-        TextView body=findViewById(R.id.detailsBody);
+        String taskBody = getIntent().getStringExtra("taskBody");
+        TextView body = findViewById(R.id.detailsBody);
         body.setText(taskBody);
 
-        String taskState=getIntent().getStringExtra("taskState");
-        TextView state=findViewById(R.id.detailsState);
+        String taskState = getIntent().getStringExtra("taskState");
+        TextView state = findViewById(R.id.detailsState);
         state.setText(taskState);
     }
-
+}
 
